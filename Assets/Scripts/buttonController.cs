f86435a4197f49081bf77f7309477e6d8c448c20
ZayZizzurp <zker49@gmail.com>
2018-10-18 20:19:30 -0400

    Created a Title scene. created a How to play scene. Used art assets to decorate title and how to play scenes. GAME IS COMPLETE!

---
+++ Assets/Scripts/buttonController.cs
@@ -0,0 +1,31 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+using UnityEngine.SceneManagement;
+
+public class buttonController : MonoBehaviour
+{
+
+	public Button StartButton, HowToPlayButton;
+
+	void Start()
+	{
+		Button btn1 = StartButton.GetComponent<Button>();
+		Button btn2 = HowToPlayButton.GetComponent<Button>();
+
+		//Calls the TaskOnClick/TaskWithParameters method when you click the Button
+		btn1.onClick.AddListener(TaskOnClick);
+		btn2.onClick.AddListener(delegate {TaskWithParameters("Hello"); });
+	}
+
+	void TaskOnClick()
+	{
+		SceneManager.LoadScene("level1");
+	}
+
+	void TaskWithParameters(string message)
+	{
+		SceneManager.LoadScene("HowToPlay");
+	}
+}
---
+++ Assets/Scripts/buttonController2.cs
@@ -0,0 +1,27 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+using UnityEngine.SceneManagement;
+
+public class buttonController2 : MonoBehaviour
+{
+
+	public Button backbutton;
+
+	void Start()
+	{
+		Button btn1 = backbutton.GetComponent<Button>();
+	
+
+		//Calls the TaskOnClick/TaskWithParameters method when you click the Button
+		btn1.onClick.AddListener(TaskOnClick);
+	}
+
+	void TaskOnClick()
+	{
+		SceneManager.LoadScene("Title");
+	}
+
+	
+}
